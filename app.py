3c4aeedc89448c1fb14c0fa3d56590163289cc99
Amruth Gudigar <amruthgudigar111@gmail.com>
2025-06-27 09:22:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,5 +35,6 @@ def run():
     return jsonify({'output': result.stdout.decode() + result.stderr.decode()})
 
 if __name__ == '__main__':
+    import os
     os.makedirs('temp', exist_ok=True)
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
